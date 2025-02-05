2f5b250bf8b98ecf30eeee63736fe89d74aba057
Jonas Tirona <Jonastirona1@gmail.com>
2025-02-04 23:32:54 -0500

    feat: add audio streaming

--- camera/views.py
+++ camera/views.py
@@ -1,14 +1,24 @@
 from django.http import StreamingHttpResponse
 from camera.utils.camera_utils import VideoCamera
+from camera.utils.audio_utils import AudioStream
+from django.shortcuts import render
 
-def gen(camera):
+def gen(camera, audio):
     while True:
-        frame = camera.get_frame()
-        if frame is None:
+        video_frame = camera.get_frame()
+        audio_frame = audio.get_frame()
+
+        if video_frame is None or audio_frame is None:
             break
+
         yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+               b'Content-Type: image/jpeg\r\n\r\n' + video_frame + b'\r\n\r\n'
+               b'--audio\r\n'
+               b'Content-Type: audio/wav\r\n\r\n' + audio_frame + b'\r\n\r\n')
+
+def stream_page(request):
+    return render(request, 'stream.html')
 
 def video_feed(request):
-    return StreamingHttpResponse(gen(VideoCamera()),
+    return StreamingHttpResponse(gen(VideoCamera(), AudioStream()),
                     content_type='multipart/x-mixed-replace; boundary=frame')
--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from camera.views import video_feed
+from camera.views import stream_page, video_feed
 
 urlpatterns = [
+    path('', stream_page, name='stream_page'),
     path('video_feed', video_feed, name='video_feed'),
 ]
