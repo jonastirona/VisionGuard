6ff5f52fcb9659fe0afd5bf76c6622c657725a2f
Jonas Tirona <Jonastirona1@gmail.com>
2025-02-09 13:14:13 -0500

    remove unneccessary files

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from camera.views import video_feed
 
 urlpatterns = [
+    path('', video_feed, name='video_feed'),
     path('video_feed', video_feed, name='video_feed'),
 ]
